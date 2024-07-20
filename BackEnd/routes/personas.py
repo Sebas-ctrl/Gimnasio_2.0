7a8059ea3d336b75b04338c638764e585c429e8b
Sebas-ctrl <210505@utxicotepec.edu.mx>
2024-07-19 19:56:13 -0600

    Depends in Personas and User

--- BackEnd/routes/personas.py
+++ BackEnd/routes/personas.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from cryptography.fernet import Fernet
 import crud.personas, config.db, schemas.personas, models.personas
 from typing import List
+from portadortoken import Portador
 
 key = Fernet.generate_key()
 f = Fernet(key)
@@ -18,13 +19,13 @@ def get_db():
         db.close()
 
 # Ruta para obtener todos los Personas
-@persona.get('/personas/', response_model=List[schemas.personas.Persona],tags=['Personas'])
+@persona.get('/personas/', response_model=List[schemas.personas.Persona],tags=['Personas'],dependencies=[Depends(Portador())] )
 def read_personas(skip: int=0, limit: int=10, db: Session=Depends(get_db)):
     db_personas = crud.personas.get_personas(db=db,skip=skip, limit=limit)
     return db_personas
 
 # Ruta para obtener un Persona por ID
-@persona.post("/person/{id}", response_model=schemas.personas.Persona, tags=["Personas"])
+@persona.post("/person/{id}", response_model=schemas.personas.Persona, tags=["Personas"],dependencies=[Depends(Portador())] )
 def read_person(id: int, db: Session = Depends(get_db)):
     db_person= crud.personas.get_persona(db=db, id=id)
     if db_person is None:
@@ -40,7 +41,7 @@ def create_person(person: schemas.personas.PersonaCreate, db: Session=Depends(ge
     return crud.personas.create_persona(db=db, person=person)
 
 # Ruta para actualizar un Persona
-@persona.put('/personas/{id}', response_model=schemas.personas.Persona,tags=['Personas'])
+@persona.put('/personas/{id}', response_model=schemas.personas.Persona,tags=['Personas'],dependencies=[Depends(Portador())] )
 def update_personas(id:int,person: schemas.personas.PersonaUpdate, db: Session=Depends(get_db)):
     db_personas = crud.personas.update_persona(db=db, id=id, person=person)
     if db_personas is None:
@@ -48,7 +49,7 @@ def update_personas(id:int,person: schemas.personas.PersonaUpdate, db: Session=D
     return db_personas
 
 # Ruta para eliminar un Persona
-@persona.delete('/personas/{id}', response_model=schemas.personas.Persona,tags=['Personas'])
+@persona.delete('/personas/{id}', response_model=schemas.personas.Persona,tags=['Personas'],dependencies=[Depends(Portador())] )
 def delete_person(id:int, db: Session=Depends(get_db)):
     db_personas = crud.personas.delete_persona(db=db, id=id)
     if db_personas is None:
